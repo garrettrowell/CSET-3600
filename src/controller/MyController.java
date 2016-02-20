bfc05eead1a77eb4da19bac5c99fafbbd277130a
Garrett Rowell <garrettrowell@gmail.com>
2016-02-19 21:30:19 -0500

    draw on canvas + resizable canvas

--- src/controller/MyController.java
+++ src/controller/MyController.java
@@ -6,18 +6,28 @@ import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
 
 public class MyController implements Initializable {
+	@FXML
+	TextArea textEditor;
+	@FXML
+	TabPane tabPane;
+	@FXML
+	Canvas canvas;
+	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-
+		//Creates Listeners for Height and Width of the canvas
+		canvas.widthProperty().addListener(observable -> drawGraphical());
+		canvas.heightProperty().addListener(observable -> drawGraphical());
 	}
-
-	@FXML
-	TextArea textEditor;
+	
 
 	@FXML
 	private void fileClose() {
@@ -44,4 +54,23 @@ public class MyController implements Initializable {
 	private void fileSave() {
 
 	}
+	
+	@FXML
+	private void drawGraphical() {
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+
+		//resize canvas to the width and height of the TabPane
+		canvas.widthProperty().bind(tabPane.widthProperty());
+		canvas.heightProperty().bind(tabPane.heightProperty());
+
+		gc.setFill(Color.BLUE);
+		gc.fillRect(100, 100, 100, 100);
+		gc.setFill(Color.RED);
+		gc.fillOval(100, 400, 100, 100);
+		gc.setFill(Color.AQUAMARINE);
+		gc.fillOval(550, 900, 100, 100);
+
+		
+	}
+	
 }
