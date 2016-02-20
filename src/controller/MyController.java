ad7c8749ccaf656d6cc1e1d0bf1ed5a337974d04
Garrett Rowell <garrettrowell@gmail.com>
2016-02-20 17:10:49 -0500

    scrollbar actually will redraw the canvas making it appear that the canvas is scrolling. This code is beyond messy and needs cleaning

--- src/controller/MyController.java
+++ src/controller/MyController.java
@@ -68,48 +68,73 @@ public class MyController implements Initializable {
 	@FXML
 	private void scrollXAxis() {
 		System.out.println("im scrolling x axis " + scrollX.getValue());
+		drawGraphical();
 	}
 	
 	@FXML
 	private void scrollYAxis() {
 		System.out.println("im scrolling y axis " + scrollY.getValue());
+		drawGraphical();
 	}
 	
 	@FXML
 	private void drawGraphical() {
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 
-		//resize canvas to the width and height of the TabPane
+		//resize canvas to the width and height of the AnchorPane
 		canvas.widthProperty().bind(graphAnchor.widthProperty());
 		canvas.heightProperty().bind(graphAnchor.heightProperty());
+		//Clear the canvas
+		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 		
 		System.out.println("Number of Vm's Present "+application.Data.vmMap.keySet().size());
 		System.out.println("Number of Hub's Present "+application.Data.hubMap.keySet().size());
+		
+		int maxX=100;
+		int maxY=100;
+		
+		//Draw a Blue rectangle for each hub
 		int hubx = 50;
 		int huby = 50;
 		for(int i=0; i<=application.Data.hubMap.keySet().size()-1; i++){
 			//hubx=hubx+100;
+			//offsets each hub vertically so they don't overlap
 			if (i>0) {
 			  huby=huby+150;
 			}
 			gc.setFill(Color.BLUE);
-			gc.fillRect(hubx, huby, 100, 100);
+			gc.fillRect(hubx-scrollX.getValue(), huby-scrollY.getValue(), 100, 100);
 
 		}
 		
+		//Draw a Red rectangle for each vm
 		int vmx = 200;
 		int vmy = 50;
 		for(int i=0; i<=application.Data.vmMap.keySet().size()-1; i++){
 			//hubx=hubx+100;
+			//offsets each vm vertically so they don't overlap
 			if (i>0) {
 				vmy=vmy+150;
 			}
 			gc.setFill(Color.RED);
-			gc.fillRect(vmx, vmy, 100, 100);
+			gc.fillRect(vmx-scrollX.getValue(), vmy-scrollY.getValue(), 100, 100);
 
 		}
-
+		
+		// determines which x&y value for vm or hub is larger
+		if (hubx > vmx) {
+			maxX=hubx-150;
+		} else {
+			maxX=vmx-150;
+		}
+		if (huby > vmy) {
+			maxY=huby-150;
+		} else {
+			maxY=vmy-150;
+		}
+		// Sets the max amount the scrollbar can recognize
+		scrollX.setMax(maxX);
+		scrollY.setMax(maxY);
 		
 	}
-	
 }
