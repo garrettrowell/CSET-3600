35ed1379351e9dc26e054a9804308aebbc2b7f7f
Garrett Rowell <garrettrowell@gmail.com>
2016-02-20 16:14:26 -0500

    tweak gui + add ScrollBar\'s to Graphical tab. currently dont scroll the canvas but listeners are setup'

--- src/controller/MyController.java
+++ src/controller/MyController.java
@@ -8,8 +8,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 
 public class MyController implements Initializable {
@@ -19,13 +21,21 @@ public class MyController implements Initializable {
 	TabPane tabPane;
 	@FXML
 	Canvas canvas;
+	@FXML
+	AnchorPane graphAnchor;
+	@FXML
+	ScrollBar scrollX;
+	@FXML
+	ScrollBar scrollY;
 	
-
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		//Creates Listeners for Height and Width of the canvas
-		canvas.widthProperty().addListener(observable -> drawGraphical());
-		canvas.heightProperty().addListener(observable -> drawGraphical());
+		graphAnchor.widthProperty().addListener(observable -> drawGraphical());
+		graphAnchor.heightProperty().addListener(observable -> drawGraphical());
+		//ScrollBar Listeners
+		scrollX.valueProperty().addListener(observable -> scrollXAxis());
+		scrollY.valueProperty().addListener(observable -> scrollYAxis());
 	}
 	
 
@@ -56,12 +66,22 @@ public class MyController implements Initializable {
 	}
 	
 	@FXML
+	private void scrollXAxis() {
+		System.out.println("im scrolling x axis " + scrollX.getValue());
+	}
+	
+	@FXML
+	private void scrollYAxis() {
+		System.out.println("im scrolling y axis " + scrollY.getValue());
+	}
+	
+	@FXML
 	private void drawGraphical() {
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 
 		//resize canvas to the width and height of the TabPane
-		canvas.widthProperty().bind(tabPane.widthProperty());
-		canvas.heightProperty().bind(tabPane.heightProperty());
+		canvas.widthProperty().bind(graphAnchor.widthProperty());
+		canvas.heightProperty().bind(graphAnchor.heightProperty());
 		
 		System.out.println("Number of Vm's Present "+application.Data.vmMap.keySet().size());
 		System.out.println("Number of Hub's Present "+application.Data.hubMap.keySet().size());
